b6ccc3f49cd06bcb581d1f7c60013ecd74e9c5c5
soldate <soldate@gmail.com>
2020-09-15 22:20:17 -0300

    self update

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,9 @@ MAINTAINER Marconi Soldate
 RUN cd /
 RUN apt update
 RUN apt install -y git
+ADD https://www.unixtimestamp.com /tmp
 RUN git clone https://github.com/soldate/mini-bitcoin.git
 RUN cd /mini-bitcoin
-WORKDIR /mini-bitcoin
 RUN javac -cp ./src/ ./src/mbtc/*.java -d ./bin
+WORKDIR /mini-bitcoin
 ENTRYPOINT java -cp ./bin mbtc.Main
--- src/mbtc/Update.java
+++ src/mbtc/Update.java
@@ -7,11 +7,13 @@ public class Update {
 	// git update and run it again
 	public static void main(final String[] args) throws IOException, InterruptedException {
 		Process p = null;
+		U.d(1, "updating...");
 		p = Runtime.getRuntime().exec("git fetch origin");
 		p.waitFor();
 		p = Runtime.getRuntime().exec("git reset --hard origin/master");
 		p.waitFor();
 		Runtime.getRuntime().exec("java -cp ./bin mbtc.Main");
+		U.d(1, "running again...");
 	}
 
 }
