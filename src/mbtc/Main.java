c30b8185c78a3146878dc296d6bea2ed5bf0bab7
soldate <soldate@gmail.com>
2020-09-17 22:56:55 -0300

    self update not working

--- src/mbtc/Main.java
+++ src/mbtc/Main.java
@@ -131,7 +131,7 @@ public class Main {
 		final long secondsFromLastRequest = (now - N.lastRequest) / 1000;
 		final long secondsFromStartTime = (now - startTime) / 1000;
 
-		if (secondsFromStartTime > 600) throw new Exception("update");
+		// if (secondsFromStartTime > 600) throw new Exception("update");
 
 		if (secondsFromLastBlock > 10 && !startMining) {// && N.amIConnected()) { // More than 10s without receive
 														// blocks
