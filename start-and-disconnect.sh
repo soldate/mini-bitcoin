d70c71c226d3d32267fc71e60307f25a25a4d95e
soldate <soldate@gmail.com>
2020-09-21 12:07:06 -0300

    no nohup

--- start-and-disconnect.sh
+++ start-and-disconnect.sh
@@ -3,4 +3,4 @@
 # https://stackoverflow.com/questions/52394419/creating-a-nohup-process-in-java
 # obviously tweak this as necessary to get the behavior you want,
 # such as redirecting output or using disown instead of nohup
-nohup java -cp ./bin mbtc.Main &
+java -cp ./bin mbtc.Main
